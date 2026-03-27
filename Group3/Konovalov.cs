495f8eb8e3fd7ea096c5d616ff7ee69aff720e3d
Konstantin <H1dra5@yandex.ru>
2026-03-27 10:10:37 +0700

    Полностью допилил реализацию, теперь выводит всё как в Burmin.cs

--- Group3/Konovalov.cs
+++ Group3/Konovalov.cs
@@ -7,10 +7,9 @@ using System.Threading.Tasks;
 
 namespace ForkKnights.Group3 {
   public class Konovalov : IKnight {
-    string name, power, fromUniverse, species, status;
-    string mage{get; set;}
-    int age;
-    bool isHeAHero;
+    public string name, power, fromUniverse, species, status;
+    public string mage{get; set;}
+    public int age;
 
   public Konovalov() {
       mage = "Последний оплот";
@@ -20,14 +19,15 @@ namespace ForkKnights.Group3 {
       species = "Человек, рождённый в слиянии дракона и человека";
       status = "Является выходцем из богатой семьи";
       age = 15;
-      isHeAHero = false;
     }
 
     public string GetJobApplication() {
       string background = $@"Он был рождён во времена драконов,{name}, {fromUniverse}, тут не ценят героев, драконы давно захватили эти земли" +
                           "и у людей не осталось надежды, из-за нехватки людей драконы стали размножаться с людьми, чтобы использовать их рабочую силу по максимуму" +
                           $@"и наш {name} {species}, он {power}, но это сыграет роль в будущем, из-за того, его мать страдает от отца Прафизизауруса он ненавидит драконов," +
-                          $@"так как он {status} это позволяет ему изучать магию у самых лучших магов своего континента, являясь {age}-летним";
+                          $@"так как он {status} это позволяет ему изучать магию у самых лучших магов своего континента, являясь {age}-летним, хоть он и ненавидит драконов, но " +
+                          "людей он тоже недолюбливает из-за их безвольности и слабости, он презирает их нежелание изменить мир и свою жизнь, потому он решил, что сам всё" +
+                          "поменяет";
       return background;
     }
   }
