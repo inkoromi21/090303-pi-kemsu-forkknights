0166b57b58ba799805f0279a6eedc46253c630b1
RudDavid <satorugodze17@gmail.com>
2026-03-29 21:50:52 +0700

    Добавил небольшую предысторию

--- Group3/Rudnev.cs
+++ Group3/Rudnev.cs
@@ -7,20 +7,36 @@ using System.Threading.Tasks;
 namespace ForkKnights.Group3 {
   internal class Rudnev : IKnight{
 
-    string name;
-    string specialization;
-    int amountOfMana;
-    int Damage;
-    int age;
+    public int hilPoints;
+    public string name;
+    public string specialization {  get; set; }
+    public int amountOfMana;
+    public int damage;
+    public int age;
 
     public Rudnev() {
+
+      hilPoints = 70;
       name = "Кллиан - древнее зло";
-      specialization = "Архимаг - ";
+      specialization = "Архимаг";
+      amountOfMana = 150;
+      damage = 40;
+      age = 1863;
     }
 
     public string GetJobApplication() {
 
-      return "test 1.0";
+      string background;
+
+      background = "Здравствуй путник, это ты оставлял объявление о поиске мага?. \n" +
+                   $"Я {specialization} и владыка некогда разрушенной магической башни и имя мне {name}. " +
+                   $"Что? Я слишком молодо выгляжу для такого заявления? Спасибо, однако мне уже {age} года. Это особенность владыки магической башни. " +
+                   "После смерти душа перерождается и постепенно вспоминает кем она была, да и живем мы довольно долго. " +
+                   "Это из-за того что когда-то очень давно первый владыка побывал у мирового древа и даже попробовал его плоды" +
+                   "От того и продолжительность жизни с молодостью и огромный запас маны" +
+                   "Однако деньги мне тоже нужны вот и подрабатываю, помогая таким путникам как ты." +
+                   "Если тебя всё устраивает, то я готов приступить к выполнению заказа прямо сейчас.";
+      return background;
 
     }
 
